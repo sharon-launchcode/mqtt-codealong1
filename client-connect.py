da6990440a8eb65aac6f97be1b3757acb455e458
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-03-29 12:20:06 -0400

    Update client-connect.py
    Add "on disconnect" message in three parts

--- client-connect.py
+++ client-connect.py
@@ -9,7 +9,11 @@ def on_connect(client, userdata, flags, rc):
         print("Connected OK")
     else:
         print("Bad connection Returned code=",rc)
-
+#Start Add disconnect message part one used by part two below
+def on_disconnect(client, userdata, flags, rc=0):
+        print(Disconnected result code " +str(rc))
+#End add disconnect message part one
+              
 #broker="test.mosquitto.org"
 #broker="broker.heivemq.com"
 #broker="iot.eclipse.org"
@@ -22,13 +26,17 @@ broker="192.168.1.206"
 
 client = mqtt.Client("python1")  #create new instance
 client.on_connect=on_connect #blind call back function
+#Start Add disconnect message part two
+client.on_disconnect=on_disconnect
+#End Add disconnect message part two             
 client.on_log=on_log
 
 print("Connecting to broker ", broker)
 client.connect(broker) #connect to broker
-#Start Add loop integrate time.sleep(4)
 client.loop_start() #Start loop
+#Start Add disconnect message part three
+client.publish("house/sensor1","my first message")
+#End Add disconnect message part three              
 time.sleep(4)
 client.loop_stop() #Stop loop
-#End Add loop integrating time.sleep(4)
 client.disconnect() #disconnect
