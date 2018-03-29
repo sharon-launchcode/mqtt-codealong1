be7f91173d88c110b48ea55766238b514c4590a4
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-03-29 10:58:40 -0400

    Create client-connect.py
    Code along, with added notes, from: How to Use the Paho Python MQTT Client for Beginners
    https://www.youtube.com/watch?v=QAaXNt0oqSI

---
+++ client-connect.py
@@ -0,0 +1,19 @@
+#!python3
+import paho.mqtt.client as mqtt #import the client1
+import time
+#ATTENTION TIME
+
+#broker="test.mosquitto.org"
+#broker="broker.heivemq.com"
+#broker="iot.eclipse.org"
+
+broker="192.168.1.206"
+#ATTENTION IP address
+
+client = mqtt.Client("python1")  #create new instance
+print("Connecting to broker ", broker)
+client.connect(broker) #connect to broker
+
+time.sleep(4)
+
+client.disconnect() #disconnect
