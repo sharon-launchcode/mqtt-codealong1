1ab19c99e8cfd71002e49da4e49c654c124f9e48
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-03-29 12:25:18 -0400

    Update client-connect.py
    Subscribe to the topic so that you can see the published messages

--- client-connect.py
+++ client-connect.py
@@ -9,10 +9,8 @@ def on_connect(client, userdata, flags, rc):
         print("Connected OK")
     else:
         print("Bad connection Returned code=",rc)
-#Start Add disconnect message part one used by part two below
 def on_disconnect(client, userdata, flags, rc=0):
         print(Disconnected result code " +str(rc))
-#End add disconnect message part one
               
 #broker="test.mosquitto.org"
 #broker="broker.heivemq.com"
@@ -26,17 +24,16 @@ broker="192.168.1.206"
 
 client = mqtt.Client("python1")  #create new instance
 client.on_connect=on_connect #blind call back function
-#Start Add disconnect message part two
-client.on_disconnect=on_disconnect
-#End Add disconnect message part two             
+client.on_disconnect=on_disconnect            
 client.on_log=on_log
 
 print("Connecting to broker ", broker)
 client.connect(broker) #connect to broker
 client.loop_start() #Start loop
-#Start Add disconnect message part three
-client.publish("house/sensor1","my first message")
-#End Add disconnect message part three              
+#Start add subscription
+client.subscribe("house/sensor1")
+#End add subscription
+client.publish("house/sensor1","my first message")           
 time.sleep(4)
 client.loop_stop() #Stop loop
 client.disconnect() #disconnect
