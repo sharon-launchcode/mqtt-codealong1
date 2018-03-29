64dc4f9847c2b84cd64d462dae1d50ef07c24eba
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-03-29 12:32:25 -0400

    Update client-connect.py
    Put in on message callback in two parts
    IMPORTANT Line 17 is not complete

--- client-connect.py
+++ client-connect.py
@@ -11,7 +11,12 @@ def on_connect(client, userdata, flags, rc):
         print("Bad connection Returned code=",rc)
 def on_disconnect(client, userdata, flags, rc=0):
         print(Disconnected result code " +str(rc))
-              
+#Start Add on_message callback part one  IMPORTANT line 17 is not complete            
+def on_message(client, userdata, msg):
+        topic=msg.topic
+        m_decode=str(msg.payload.decode("utf-8", "ignore")
+        print("message received", m_decode)                                
+#End Add message callback part one IMPORTANT Line 17 is not complete              
 #broker="test.mosquitto.org"
 #broker="broker.heivemq.com"
 #broker="iot.eclipse.org"
@@ -26,13 +31,13 @@ client = mqtt.Client("python1")  #create new instance
 client.on_connect=on_connect #blind call back function
 client.on_disconnect=on_disconnect            
 client.on_log=on_log
-
+#Start add on_message callback part two                     
+client.on_message=on_message
+#End add on message callback part two                     
 print("Connecting to broker ", broker)
 client.connect(broker) #connect to broker
 client.loop_start() #Start loop
-#Start add subscription
 client.subscribe("house/sensor1")
-#End add subscription
 client.publish("house/sensor1","my first message")           
 time.sleep(4)
 client.loop_stop() #Stop loop
